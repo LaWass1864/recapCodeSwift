708df66dfa7abedf7e72fec7c0671d568b43abcb
LaWass1864 <amwass4@hotmail.com>
2023-10-25 17:35:37 +0200

    Initial Commit

---
+++ recapCode/recapCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  recapCodeApp.swift
+//  recapCode
+//
+//  Created by Apprenant 76 on 25/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct recapCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
